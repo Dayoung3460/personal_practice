7147adf9aab1f3bfb13c1d4b3357555ad22be678
Dayoung Kim <kimdayoung771@gmail.com>
2021-03-14 18:29:41 +0900

    vue and websocket

---
+++ js/app.ts
@@ -0,0 +1,19 @@
+let studentID: number = 12345
+let studentName: string = 'Jenny Kim'
+let age: number = 21
+let gender: string = 'female'
+let subject: string = 'javascript'
+let courseCompleted: boolean = false
+
+interface Student {
+    studentID: number
+    studentName: string
+    age: number
+    gender: string
+    subjuct: string
+    createDate: Date
+}
+
+function getStudentDetails(studentID: number) {
+    return null;
+}
